24372b4da2836336225f66c4596782b28cb26a93
Marcos Cunha <marcos.cunha1025@gmail.com>
2024-02-19 22:11:26 -0300

    Created component of footer

--- angular/TaskManager/src/app/components/home/home.component.ts
+++ angular/TaskManager/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { TableComponent } from '../table/table.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, TableComponent],
+  imports: [NavbarComponent, TableComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
